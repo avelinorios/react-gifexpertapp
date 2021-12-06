ccf86fb3f811ae7365d166ea9d61f7c0d134e381
Avelino Ríos <avelino5872@gmail.com>
2021-12-06 14:11:23 +0100

    Se convierte petición a HTTPS

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=zpSwwAHDMoEDBRVdHRxXBzHsMn5t40tN`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=zpSwwAHDMoEDBRVdHRxXBzHsMn5t40tN`;
     const response = await fetch( url );
     const {data} = await response.json();
 
